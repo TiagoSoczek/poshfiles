8c634f04689b2ab81f12d341d9b2e611e8ef4b36
Tiago Soczek <tiagso@microsoft.com>
2018-06-10 19:09:47 -0300

    Some aliases

---
+++ Aliases.ps1
@@ -0,0 +1,7 @@
+Set-Alias -Name c -Value cls
+Set-Alias -Name n -Value notepad
+
+Set-Alias -Name g -Value git
+
+Set-Alias -Name d -Value docker
+Set-Alias -Name k -Value kubectl
---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+$root = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+. "$root/Aliases.ps1"
